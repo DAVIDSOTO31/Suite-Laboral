2019745a1192a7670edd1e8e7727c3caea6a0ca4
DAVIDSOTO31 <davidsotobuiles@gmail.com>
2026-09-25 19:34:01 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -100,7 +100,7 @@ const server = http.createServer(async (req, res) => {
     "'sha256-VHT9CPskO5vtuB4/dvdS04Q+SweVGZPL/1qWK+IKjFI='", // index.html
     "'sha256-dum9fwlx0dkN5ryTuRUUwv195GfaGUq71kgIuRd86vs='", // login.html
     "'sha256-Mw75BhCr8p7KOHnUhedU/phgSplvZ2WhY9Oiy0vo2jA='", // app.html
-    "'sha256-Vg0lkpEUaRwl1e4TJtu3+bHOQPCIrP4qkdoMWzoPCJU='", // superadmin.html
+    "'sha256-de8OXDT1qUTPoirOCv0Gv4wz3ZUKttH7eXUUqSNaEyk='", // superadmin.html
     "'sha256-acq3Igc1f3abTnmdP0vrk+f21ykK7GXxlTu0HAeDxD8='", // accept-invite.html
     "'sha256-p7WRDAOI/vt0mUjmPWtSZe+ugsLNJkiYzKuVx61YsjA='", // forgot-password.html
     "'sha256-mZ3lS9DOiVRo7hkAzBXkRD0YGddIYpBoAsC0YnepqJQ='", // reset-password.html
